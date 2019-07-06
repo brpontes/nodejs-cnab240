2239a6b7b0a9fc17a5ed0d2244e1127d54f50bcb
Bruno Pontes <brunodeveloper18@gmail.com>
2019-07-06 17:17:15 -0300

    Add profile prop

--- example/index.js
+++ example/index.js
@@ -1,8 +1,21 @@
 const cnab240 = require('../index.js')
 const fs = require('fs')
-const datasource_pagamento = require('./datasource_pag')
+const datasource = require('./datasource')
+
+const company = {
+    COD_BANCO: '001',
+    TIPO_INSCRICAO: '2',
+    INSCRICAO: '03377700000198',
+    CONVENIO: '3061856',
+    //CONVENIO_PAG: '0007709780126',
+    CARTEIRA: '17',
+    VARIACAO_CARTEIRA: '051',
+    AGENCIA: '28126',
+    NUMERO_CONTA: '634107',
+    NOME_EMPRESA: 'BP SMART TECH',
+}
 
 cnab240
-    .init( datasource_pagamento, 'PAGAMENTO' )
+    .init( company, datasource, 'COBRANCA' )
     .then( res => fs.writeFileSync('/home/bruno/workspace/TESTE.txt', res) )
     .catch( err => console.log(err) )
