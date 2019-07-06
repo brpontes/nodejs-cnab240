32cd8128ddef09c24b1455069e359337e259e38e
Bruno Pontes <brunodeveloper18@gmail.com>
2019-07-06 10:54:37 -0300

    Change example folder name

---
+++ example/index.js
@@ -0,0 +1,9 @@
+const cnab240 = require('../index.js')
+const fs = require('fs')
+const datasource = require('./datasource')
+const datasource_pagamento = require('./datasource_pag')
+
+cnab240
+    .init( datasource_pagamento, 'PAGAMENTO' )
+    .then( res => fs.writeFileSync('/home/bruno/workspace/TESTE.txt', res) )
+    .catch( err => console.log(err) )
